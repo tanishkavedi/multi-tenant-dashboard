c1b973c736e9d39feeeedfa6749d1856d9c22568
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-05-28 16:01:51 +0530

    connect auth routes to Express app

--- server/app.js
+++ server/app.js
@@ -3,10 +3,16 @@ const express = require('express')
 const cors    = require('cors')
 require('dotenv').config()
 
+const authRoutes = require('./routes/auth')
+
+
 const app = express()
 
 app.use(cors())
-app.use(express.json())  
+app.use(express.json()) 
+
+
+app.use('/api/auth', authRoutes)
 
 
 app.get('/', (req, res) => {
