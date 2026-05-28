171ad9e47d267fe4de89316e5b75fecb0ca6946a
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-05-28 18:51:27 +0530

    Wrap routes in App.js with ProtectedRoute

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Members   from './pages/Members'
+import ProtectedRoute from './components/ProtectedRoute'
+
 
 export default function App() {
   return (
@@ -10,8 +12,15 @@ export default function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-         <Route path="/members" element={<Members />} />
+        <Route path="/dashboard" element={<ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>} />
+          <Route path="/members" element={
+          <ProtectedRoute allowedRoles={['owner', 'admin']}>
+            <Members />
+          </ProtectedRoute>
+        } />
+
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
